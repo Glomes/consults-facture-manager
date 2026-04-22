0ed9fcf91fbe2cc8683b8725f2424f60cef3500d
Lucas Gomes Mendes Icassatti <111081582+Glomes@users.noreply.github.com>
2026-04-22 19:07:11 -0400

    ...

--- src/server.ts
+++ src/server.ts
@@ -27,9 +27,9 @@ app.use(cors({
 
 app.use(routes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = Number(process.env.PORT) || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server rodando na porta ${PORT}`);
 });
 
