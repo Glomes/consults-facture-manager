d00e4a9c32b7b1ad740a91e8378421e500bd4761
Lucas Gomes Mendes Icassatti <111081582+Glomes@users.noreply.github.com>
2026-04-24 14:25:15 -0400

    refactor: tratamento de erros

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -10,21 +10,22 @@ export const AuthController = {
   async register(req: Request, res: Response) {
     const { nome, email, senha } = req.body;
     const hash = await bcrypt.hash(senha, 10);
-    
+
     try {
       const { rows } = await pool.query(
         'INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3) RETURNING id, nome, email',
         [nome, email, hash]
       );
       return res.status(201).json(rows[0]);
-    } catch (error) {
-      return res.status(400).json({ error: "E-mail já cadastrado" });
+    } catch (error: any) {
+      console.error("ERRO REAL REGISTER:", error); // 👈 AQUI
+      return res.status(400).json({ error: error.message });
     }
   },
 
   async login(req: Request, res: Response) {
     const { email, senha } = req.body;
-    
+
     try {
       const { rows } = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
       const user = rows[0];
@@ -35,10 +36,10 @@ export const AuthController = {
 
       // Agora o TS aceita o JWT_SECRET sem reclamar de undefined
       const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '7d' });
-      
-      return res.json({ 
-        user: { id: user.id, nome: user.nome }, 
-        token 
+
+      return res.json({
+        user: { id: user.id, nome: user.nome },
+        token
       });
     } catch (error) {
       return res.status(500).json({ error: "Erro interno no servidor" });
